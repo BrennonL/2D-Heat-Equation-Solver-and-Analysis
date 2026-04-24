080631ad5d0629a88de50085a0aca4cbe73d703d
haticenursun <haticenursun@vt.edu>
2026-04-24 14:28:41 -0400

    add exact solution equation

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 import numpy as np
+import matplotlib.pyplot as plt
+
+def exact_solution(X, Y, t, alpha, Lx, Ly):
+    return (
+        np.sin(np.pi * X / Lx)
+        * np.sin(np.pi * Y / Ly)
+        * np.exp(-alpha * np.pi**2 * (1 / Lx**2 + 1 / Ly**2) * t)
+    )
 
 def main():
   ...
